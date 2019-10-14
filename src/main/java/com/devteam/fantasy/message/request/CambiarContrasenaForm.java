8579975e9bd950473a1e6c2080785217e5f0b561
Cristian.ruiz <cristian.ruiz@contractors.accruent.com>
2019-10-14 07:39:24 -1000

    cambiar contrasena response object

---
+++ src/main/java/com/devteam/fantasy/message/request/CambiarContrasenaForm.java
@@ -0,0 +1,15 @@
+package com.devteam.fantasy.message.request;
+
+public class CambiarContrasenaForm extends UserIdForm{
+
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
