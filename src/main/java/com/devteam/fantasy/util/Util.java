8bf8fd46cb339ddbc5e7eb08432050ad0a9ffa59
Cristian.ruiz <cristian.ruiz@contractors.accruent.com>
2019-11-03 08:16:08 -1000

    delete ticos implementation for closing sorteos

--- src/main/java/com/devteam/fantasy/util/Util.java
+++ src/main/java/com/devteam/fantasy/util/Util.java
@@ -56,7 +56,7 @@ public class Util {
 
 	private static final Logger log = LoggerFactory.getLogger(Util.class);
     private static TimeDate time = TimeDate.getInstance();
-    private static LocalDate ld = LocalDate.now(time.getZ());
+    
     private static String[] months = {"Enero", "Febrero", "Marzo", "Abril",
             "Mayo", "Junio", "Julio",
             "Agosto", "Septiembre", "Octubre",
@@ -111,6 +111,7 @@ public class Util {
                                     SorteoTypeRepository sorteoTypeRepository,
                                     SorteoTypeName sorteoTypeName,
                                     int hour) {
+    	LocalDate ld = LocalDate.now(time.getZ());
         Sorteo sorteo = new Sorteo();
         sorteo.setEstado(estadoRepository.getEstadoByEstado(EstadoName.ABIERTA));
 
@@ -171,6 +172,7 @@ public class Util {
                                     SorteoRepository sorteoRepository,
                                     SorteoDiariaRepository sorteoDiariaRepository,
                                     int hour) {
+    	LocalDate ld = LocalDate.now(time.getZ());
         SorteoDiaria sorteoDiaria = sorteoDiariaRepository.getSorteoActivoBySorteoTime(new Timestamp(
                 ZonedDateTime.of(ld.getYear(), ld.getMonthValue(), ld.getDayOfMonth(), hour, 0, 0, 0,
                         ZoneId.of("America/Tegucigalpa")
@@ -189,41 +191,26 @@ public class Util {
                                     SorteoDiariaRepository sorteoDiariaRepository,
                                     int hour) throws Exception {
     	
-    	log.debug("DIARIA: hour["+hour+"] Intento 1");
-    	log.info("DIARIA: hour["+hour+"] Intento 1");
-    	Timestamp ticosHour = new Timestamp(
-                ZonedDateTime.of(ld.getYear(), ld.getMonthValue(), ld.getDayOfMonth(), hour, 0, 0, 0,
-                        ZoneId.of("America/Tegucigalpa")
-                ).toInstant().toEpochMilli()
-        );
-    	log.debug("TicosHour: ["+ticosHour.toString()+"]");
-    	log.info("TicosHour: ["+ticosHour.toString()+"]");
-        SorteoDiaria sorteoDiaria = sorteoDiariaRepository.getSorteoActivoBySorteoTime(ticosHour);
-        if (sorteoDiaria != null) {
+    	log.debug("DIARIA: hour["+hour+"] ");
+    	log.info("DIARIA: hour["+hour+"] ");
+    	LocalDateTime sorteoTime = LocalDateTime.now();
+    	sorteoTime = sorteoTime.withHour(hour);
+    	sorteoTime = sorteoTime.withMinute(0);
+    	sorteoTime =sorteoTime.withSecond(0);
+    	sorteoTime = sorteoTime.withNano(0);
+    	
+    	log.debug("sorteoTime to find: "+sorteoTime.toString());
+    	log.info("sorteoTime to find: "+sorteoTime.toString());
+    	SorteoDiaria sorteoDiaria = sorteoDiariaRepository.getSorteoActivoBySorteoTime(Timestamp.valueOf(sorteoTime));
+    	log.debug("Encontro sorteo ? :"+sorteoDiaria!=null?"Yes":"No");
+    	if (sorteoDiaria != null) {
             Sorteo sorteo = sorteoRepository.getSorteoById(sorteoDiaria.getId());
             sorteo.setEstado(estadoRepository.getEstadoByEstado(EstadoName.CERRADA));
             sorteoRepository.save(sorteo);
-        }else {
-        	log.debug("DIARIA: hour["+hour+"] Intento 2");
-        	log.info("DIARIA: hour["+hour+"] Intento 2");
-        	LocalDateTime sorteoTime = LocalDateTime.now();
-        	sorteoTime = sorteoTime.withHour(hour);
-        	sorteoTime = sorteoTime.withMinute(0);
-        	sorteoTime =sorteoTime.withSecond(0);
-        	sorteoTime = sorteoTime.withNano(0);
-        	
-        	log.debug("sorteoTime to find: "+sorteoTime.toString());
-        	log.info("sorteoTime to find: "+sorteoTime.toString());
-        	SorteoDiaria sorteoDiariaAux = sorteoDiariaRepository.getSorteoActivoBySorteoTime(Timestamp.valueOf(sorteoTime));
-        	log.debug("Encontro sorteo ? :"+sorteoDiaria!=null?"Yes":"No");
-        	if (sorteoDiariaAux != null) {
-                Sorteo sorteo = sorteoRepository.getSorteoById(sorteoDiariaAux.getId());
-                sorteo.setEstado(estadoRepository.getEstadoByEstado(EstadoName.CERRADA));
-                sorteoRepository.save(sorteo);
-        	}else {
-        		throw new Exception("No se pudo cerrar el sorteo de las ["+hour+"]. sorteoTime=["+sorteoTime.toString()+"]");
-        	}
-        }
+    	}else {
+    		throw new Exception("No se pudo cerrar el sorteo de las ["+hour+"]. sorteoTime=["+sorteoTime.toString()+"]");
+    	}
+        
     }
 
     public static User getUserFromJsonNode(UserRepository userRepository, ObjectNode json) {
